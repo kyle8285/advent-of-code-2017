8767076a6e09fbdbc24c66b3f4af7c268c14e972
kyle8285 <kylelussier3@gmail.com>
2018-01-01 09:42:06 -0500

    Day 2 part two complete

--- day2/day2.js
+++ day2/day2.js
@@ -2,10 +2,36 @@ exports.checksum = (spreadsheet='') => {
   return spreadsheet
     .trim()
     .split('\n')
-    .map(line => line.trim().split(/\s+/))
+    .map(line =>
+      line
+        .trim()
+        .split(/\s+/)
+        .map(num => Number(num))
+    )
     .reduce((acc, val) => {
-      val.forEach(num => Number(num))
-      acc += Math.max(...val) - Math.min(...val)
+      const diff = Math.max(...val) - Math.min(...val)
+      return acc += diff;
+    }, 0)
+};
+
+exports.checksumEvenlyDivisible = (spreadsheet='') => {
+  return spreadsheet
+    .trim()
+    .split('\n')
+    .map(line =>
+      line
+        .trim()
+        .split(/\s+/)
+        .map(num => Number(num))
+    )
+    .reduce((acc, val) => {
+      val.find(num1 => {
+        val.find(num2 => {
+          return num1 % num2 === 0
+            && num1 !== num2
+            && (acc += (num1/num2))
+        })
+      })
       return acc;
     }, 0)
 };
--- day2/day2.test.js
+++ day2/day2.test.js
@@ -1,15 +1,28 @@
-const fs           = require('fs');
-const expect       = require('chai').expect;
-const { checksum } = require('./day2');
+const fs     = require('fs');
+const expect = require('chai').expect;
+const {
+  checksum,
+  checksumEvenlyDivisible
+} = require('./day2');
 
 const exampleText  = fs.readFileSync('./example.txt', 'utf8');
-const testText     = fs.readFileSync('./input.txt', 'utf8');
+const exampleText2  = fs.readFileSync('./example2.txt', 'utf8');
 const randomString = '1 1 3\n3 4 8\n1 2  2\n\n';
+const randomString2 = '5 3 6 8\n4 5 6 10\n2 7 9 3';
+const testText     = fs.readFileSync('./input.txt', 'utf8');
 
 describe('day2', () => {
   it('part one: should perform the checksum', () => {
     expect(checksum(exampleText)).to.equal(18);
-    expect(checksum(testText)).to.equal(41887);
     expect(checksum(randomString)).to.equal(8);
+    expect(checksum(testText)).to.equal(41887);
+  });
+});
+
+describe('day2', () => {
+  it('part two: should sum evenly divisible values', () => {
+    expect(checksumEvenlyDivisible(exampleText2)).to.equal(9);
+    expect(checksumEvenlyDivisible(randomString2)).to.equal(7);
+    expect(checksumEvenlyDivisible(testText)).to.equal(226);
   });
 });
