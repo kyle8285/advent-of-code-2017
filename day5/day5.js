0aaec2dfeacf372928344e2ed41865929a8a6ed6
kyle8285 <kylelussier3@gmail.com>
2018-01-04 18:51:11 -0500

    Day 5 part two complete

--- day5/day5.js
+++ day5/day5.js
@@ -18,3 +18,26 @@ exports.jumps = seq => {
   }
   return totalSteps;
 }
+
+exports.strangeJumps = seq => {
+  // convert input to array of number instructions
+  const instructions = seq.trim().split(/\s+/).map(Number);
+
+  // set a boundary. if we try to move outside
+  // of the boundary, we've reached an exit
+  const upperBound = instructions.length - 1;
+  const lowerBound = 0;
+
+  // start at index 0
+  let currentIndex = 0;
+  let totalSteps = 0;
+  while (currentIndex >= lowerBound && currentIndex <= upperBound) {
+    let stepsToMove = instructions[currentIndex];
+    stepsToMove > 2
+      ? instructions[currentIndex]--
+      : instructions[currentIndex]++;
+    currentIndex += stepsToMove;
+    totalSteps++;
+  }
+  return totalSteps;
+}
--- day5/day5.test.js
+++ day5/day5.test.js
@@ -1,11 +1,16 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf8');
-const { jumps } = require('./day5');
+const { jumps, strangeJumps } = require('./day5');
 
 describe('day5', () => {
   it('part one: should calculate how many steps to reach the exit', () => {
     expect(jumps('0\n3\n0\n1\n-3')).to.equal(5);
     expect(jumps(input)).to.equal(364539);
+  });
+
+  it('part two: should calculate how many steps to reach the exit with strange jumps', () => {
+    expect(strangeJumps('0\n3\n0\n1\n-3')).to.equal(10);
+    expect(strangeJumps(input)).to.equal(27477714);
   });
 });
